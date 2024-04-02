bfc589a7c71311aca3d943fc0c6a3f4ea7cdb103
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-02 18:23:25 +0000

    rherhre

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateTheArea() {
+
+  // input
+  const abase = parseInt(document.getElementById('a-base').value)
+  const bbase = parseInt(document.getElementById('b-base').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  // process
+  const area = [(abase + bbase)/2]*height
+ 
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is : ' + area + 'mm²'
+}
